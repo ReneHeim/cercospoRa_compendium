1cfd9fef52d98dc1217a2397dd2799f5b230b079
Nathan Okole <119446576+nathanokole@users.noreply.github.com>
2024-06-01 17:24:29 +0200

    Add files via upload

--- code/RTM_LAI_estimation_from_S2.R
+++ code/RTM_LAI_estimation_from_S2.R
@@ -39,7 +39,7 @@ maximize_cos <- function(v, T){
 
 
 # function to retrieve LAI values at specific points for a given UAV image containig reflectance values
-retrieve_lai <- function(image_date, folder_directory){
+retrieve_lai <- function(image_date, folder_directory, sf_bonitur){
   #generate look up table (LUT)
   #define parameters for sugar beet according to 
   #Jay, Sylvain, Fabienne Maupas, Ryad Bendoula, and Nathalie Gorretta. 
@@ -69,11 +69,11 @@ retrieve_lai <- function(image_date, folder_directory){
                 Cw=Cw
   )
   
-  #spectral bands captured by Micasense Altum
-  data_resampling_matrix <- data.frame(center=c(475, 560, 668, 717, 842),
-                                       fwhm=c(32, 27, 14, 12, 57))
+  #spectral bands captured by S2 corresponding to the Micasense Altum
+  data_resampling_matrix <- data.frame(center=c(492, 560, 664, 704, 833),
+                                       fwhm=c(66, 36, 31, 15, 106))
   
-  #Resample spectra from the LUT to spectral bands captured by Micasense Altum
+  #Resample spectra from the LUT  to spectral bands captured by S2 sensors
   spect_resample <- spectralResampling(spect, data_resampling_matrix)
   
   
@@ -84,20 +84,23 @@ retrieve_lai <- function(image_date, folder_directory){
   # read raster  
   image_path <- file.path(folder_directory, paste(image_date,'.tif', sep = ''))
   allbands <- brick(image_path)
-  allbands <- allbands[[1:5]]
+  allbands <- allbands[[c(2:5, 8)]]
   
-  #aggregate spectra to the same spatial resolution as S2 (here we use a factor of 30)
-  aggregated_raster <- aggregate(allbands, fact=30, fun='mean')
+  #Crop S2 image to our region of interest (defined by a polygon shapefile) to reduce computational burden
+  sf_this_spectra <- raster::crop(allbands, sf_bonitur)
+  sf_this_spectra <- (sf_this_spectra-1000)/10000
   
-  #extract LAI at each sampling location and store it in a raster file with the same properties as the initial reflectance raster
+  #extract LAI at each sampling location and store it
   #lai_RMSE <- c()
-  lai_cos <- aggregated_raster[[1]]#lai_COS <- c()
-  for (j in 1:dim(aggregated_raster)[1]){
-    for (i in 1:dim(aggregated_raster)[2]){
-      spect <- aggregated_raster[j,i]
+  lai_cos <- sf_this_spectra[[1]]#lai_COS <- c()
+  for (j in 1:dim(sf_this_spectra)[1]){
+    for (i in 1:dim(sf_this_spectra)[2]){
+      spect <- sf_this_spectra[j,i]
       #lai_i_RMSE <- SI(spect_resample)$LAI[minimize_RMSE(spect, spectra(spect_resample))] # this in case you want the RMSE distance
       lai_i_COS <- SI(spect_resample)$LAI[maximize_cos(spect, spectra(spect_resample))]
       if(is.na(lai_i_COS[1])) lai_i_COS <- NA
+      #lai_RMSE <- c(lai_RMSE, lai_i_RMSE)
+      #lai_COS <- c(lai_COS, lai_i_COS)
       lai_cos[j,i]  <- lai_i_COS
     }  
   }
@@ -110,9 +113,15 @@ retrieve_lai <- function(image_date, folder_directory){
 
 
 
+
+
 # Start the actual job
+# set and read shapefile path used to crop the S2 image
+sf_path <- r"(D:\cercospoRa\raw\scoring\SE04_field_perimeter.gpkg)"
+sf_field_perimeter <- read_sf(sf_path)
+
 # find list of images
-folder_directory <- r"(D:\cercospoRa\uav\22)"
+folder_directory <- r"(D:\cercospoRa\raw\S2A)"
 image_list <- list.files(folder_directory, recursive = FALSE)
 image_list <- image_list[endsWith(image_list, '.tif')]
 image_date_list <- strsplit(image_list, ".tif")
@@ -133,7 +142,7 @@ no_cores <- min(5, no_cores)
 
 # Initialize cluster for parallel computing
 cl <- makeCluster(no_cores)
-clusterExport(cl=cl, varlist=c("retrieve_lai", "tail", "maximize_cos"))
+clusterExport(cl=cl, varlist=c("retrieve_lai", "tail", "maximize_cos", "sf_field_perimeter"))
 
 parLapply(cl, 
           image_date_vector,
@@ -142,7 +151,7 @@ parLapply(cl,
             library(sf)
             library(raster)
             library("hsdar") # For RTM modeling
-            folder_directory <- r"(D:\cercospoRa\uav\22)"
-            retrieve_lai(x, folder_directory)
+            folder_directory <- r"(D:\cercospoRa\raw\S2A)"
+            retrieve_lai(x, folder_directory, sf_field_perimeter)
             })
 stopCluster(cl)
