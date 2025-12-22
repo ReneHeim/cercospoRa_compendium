7b1bb7a1e96618a75faecf1d7d7fa79df1fce2f9
Paul Melloy <paul@melloy.com.au>
2025-12-23 08:57:10 +1000

    Fix image_path indexation and remove commented code

--- analysis/00_RTM_LAI_estimation.R
+++ analysis/00_RTM_LAI_estimation.R
@@ -251,11 +251,7 @@ platforms = list(list(platform="S2", harmonized = FALSE, normalized = FALSE, ban
                                                           fwhm=c(32, 27, 14, 12, 57)))
 )
 
-# platforms = list(list(platform="UAV", harmonized = TRUE, normalized = TRUE, band_index = 1:5, aggregate = TRUE, agg_factor=105, #GSD = 1 m
-#                       data_resampling_matrix = data.frame(wl=c(475, 560, 668, 717, 842),
-#                                                           fwhm=c(32, 27, 14, 12, 57)))
-# )
-
+# Now to generate the LAI estimations and save the outputs
 
 lapply(platforms, function(pform){
 
@@ -273,7 +269,7 @@ lapply(platforms, function(pform){
   message("Start ", pform$platform," loop")
   lapply(seq_along(image_paths),function(image_n){
     retrieve_lai(image_date = image_dates[image_n],
-                 image_path = image_paths,
+                 image_path = image_paths[image_n],
                  roi = roi,
                  spect = spect,
                  data_resampling_matrix = pform$data_resampling_matrix,
@@ -287,150 +283,3 @@ lapply(platforms, function(pform){
 
 })
 
-# for(platform_ID in platforms){
-  # #unlist
-  # platform = platform_ID$platform
-  # harmonized = platform_ID$harmonized
-  # band_index = platform_ID$band_index
-  # data_resampling_matrix = platform_ID$data_resampling_matrix
-  # normalized = platform_ID$normalized
-  # aggregate = platform_ID$aggregate
-  # agg_factor = platform_ID$agg_factor
-  #
-  # # find list of images
-  # folder_directory <- file.path(here::here("data"), platform)
-  # image_list <- list.files(folder_directory, recursive = FALSE)
-  # image_list <- image_list[endsWith(image_list, '.tif')]
-  # image_date_list <- strsplit(image_list, ".tif")
-
-  # read all dates available for images
-  # image_date_vector <- c()
-  # for (i in seq_along(image_date_list)){
-  #   j <- ls_tail(image_date_list[[i]])
-  #   image_date_vector <- c(image_date_vector, j)
-  # }
-
-
-
-#}
-
-
-
-#
-# solar_zenith = 10
-# deltaazimuth = 180
-# sensor_zenith = 0
-# LAI0 = seq(0.1,6,0.01)
-# N0 = seq(1,3.5,0.5)
-# Cab0 = seq(20,65,7)
-# Cw0 = seq(0.03,0.09,0.015)
-#
-# LAI =  rep(LAI0, times = length(Cab0)*length(Cw0)*length(N0))
-# Cab = rep(rep(Cab0, each = length(LAI0)), times=length(Cw0)*length(N0))
-# Cw = rep(rep(Cw0, each = length(Cab0)*length(LAI0)), times = length(N0))
-# N = rep(N0, each = length(Cab0)*length(LAI0)*length(Cw0))
-#
-#
-# InputPROSAIL <- data.frame(
-#   N     = N,
-#   CHL   = Cab,
-#   EWT   = Cw,
-#   lai   = LAI,
-#   TypeLidf = 1, # type of leaf distribution https://jbferet.gitlab.io/prosail/reference/PRO4SAIL.html
-#   LIDFa = -0.35, # Spherical (default)	-0.35	-0.15 https://www.quantargo.com/help/r/latest/packages/hsdar/checkhull.html/PROSAIL2R
-#   LIDFb = -0.15,
-#   q = 0.01, # Hot Spot parameter
-#   psoil = 0.3, #30% dry soil
-#   tto = sensor_zenith,
-#   tts = solar_zenith,
-#   psi = deltaazimuth
-# )
-#
-# spect <- Generate_LUT_PROSAIL(SAILversion = '4SAIL',
-#                             InputPROSAIL = InputPROSAIL,
-#                             SpecPROSPECT = prospect::SpecPROSPECT_FullRange,
-#                             SpecSOIL = SpecSOIL,
-#                             SpecATM = SpecATM)
-# spect <- spect$BRF
-#
-#
-# #spectral bands captured by S2 corresponding to the Micasense Altum
-# data_resampling_matrix <- data.frame(center=c(492, 560, 664, 704, 833),
-#                                      fwhm=c(66, 36, 31, 15, 106))
-#
-#
-# # function to retrieve LAI values at specific points for a given UAV image
-# #  containing reflectance values
-# retrieve_lai <- function(image_date,
-#                          folder_directory,
-#                          roi,
-#                          spect,
-#                          data_resampling_matrix,
-#                          band_index = c(2:5, 8),
-#                          platform = "S2",
-#                          harmonized = FALSE,
-#                          normalized = FALSE,
-#                          aggregate = FALSE,
-#                          agg_factor = 50,
-#                          lai = InputPROSAIL$lai){
-#
-#   # Resample spectrum according to sensor
-#   sensor <- get_spec_sensor(
-#     SensorName = "sensor",
-#     SpectralProps = data_resampling_matrix,
-#     Path_SensorResponse = "./")
-#   SRF_sensor <- GetRadiometry("sensor")
-#
-#   spect_resample <- applySensorCharacteristics(wvl = prospect::SpecPROSPECT_FullRange$lambda,
-#                                                SRF = SRF_sensor,
-#                                                InRefl = spect)
-#
-#   rownames(spect_resample) <- SRF_sensor$Spectral_Bands
-#   spect_resample <- as.data.frame(t(spect_resample))
-#
-#   # read raster
-#   image_path <- file.path(folder_directory, paste(image_date,'.tif', sep = ''))
-#   allbands <- terra::rast(image_path)
-#   allbands <- allbands[[band_index]] #allbands[[c(2:5, 8)]]
-#
-#   # Preprocess the rasters
-#   cat(paste0("convert to SpatRaster: ", image_date, "\n"))
-#   allbands <- terra::crop(allbands, st_transform(roi, crs(allbands)), mask=TRUE)
-#   if(!harmonized) allbands <- allbands - 1000 #additive offset
-#   if(!normalized) allbands <- (allbands)/10000 # harmonized S2
-#   if(aggregate) allbands <- terra::aggregate(allbands, fact=agg_factor, fun='mean') # aggregate in case it is UAV
-#
-#
-#   # Extract spectrum and LAI
-#   spect_resample_ <- spect_resample
-#   lai <- lai
-#
-#   # Apply the function
-#   lai_inversion = terra::app(allbands, maximize_cos_wrapper(T_var=spect_resample_, lai=lai), cores = 12)
-#
-#   # Create saving path
-#   lai_folder_directory <- file.path("Data", paste0("LAI_maps4"))
-#   if(!dir.exists(lai_folder_directory)) dir.create(lai_folder_directory)
-#
-#   output_foldername <- file.path(lai_folder_directory, platform)
-#   if(!dir.exists(output_foldername)) dir.create(output_foldername)
-#   output_filename <- file.path(output_foldername, paste(image_date,'.tif', sep = ''))
-#   terra::writeRaster(lai_inversion, filename = output_filename)
-#   print(paste(output_filename, 'done',  sep = ' '))
-# }
-#
-# # cosine distance
-# maximize_cos_wrapper <- function(T_var, lai) {
-#   function(v) {
-#     vect <- unlist(v)
-#     if(any(is.na(vect))) return(NaN)
-#     cov <- as.matrix(T_var) %*% vect
-#     sqrvar1 <- sqrt(sum(vect^2))
-#     sqrvar2 <- sqrt(apply(as.matrix(T_var)^2, 1, sum))
-#     cos <- cov / (sqrvar1 * sqrvar2)
-#     max_cos <- which.max(cos)
-#     return(lai[max_cos])
-#   }
-# }
-#
-#
